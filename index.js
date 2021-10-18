16bc083d9b83eb1d211fdf3da56ab86dbfddba4f
Swapnil Sengupta <dodosp2001@gmail.com>
2021-10-18 20:34:25 +0530

    Add basic signup/login functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/user", userRouter);
+
+const CONNECTION_URL =
+  "mongodb+srv://new-Swapnil:swap24new@cluster0.owmdh.mongodb.net/codepen?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(`${error} did not connect`));
