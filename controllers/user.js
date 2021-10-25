e4e970d63ca894d7ab235ce91fae2c0f6fa89d94
Swapnil Sengupta <dodosp2001@gmail.com>
2021-10-25 17:34:28 +0530

    Modify error messages during authentication

--- controllers/user.js
+++ controllers/user.js
@@ -11,8 +11,7 @@ export const signup = async (req, res) => {
   try {
     const oldUser = await User.findOne({ $or: [{ email }, { username }] });
 
-    if (oldUser)
-      return res.status(400).json({ message: "User already exists" });
+    if (oldUser) return res.json({ message: "User already exists." });
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -20,7 +19,6 @@ export const signup = async (req, res) => {
       email,
       password: hashedPassword,
       username,
-      starred: [],
     });
 
     const token = jwt.sign({ email: result.email, id: result._id }, secret, {});
@@ -39,13 +37,12 @@ export const signin = async (req, res) => {
   try {
     const oldUser = await User.findOne({ email });
 
-    if (!oldUser)
-      return res.status(404).json({ message: "User doesn't exist" });
+    if (!oldUser) return res.json({ message: "User doesn't exist!" });
 
     const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
     if (!isPasswordCorrect)
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.json({ message: "Invalid credentials." });
 
     const token = jwt.sign(
       { email: oldUser.email, id: oldUser._id },
